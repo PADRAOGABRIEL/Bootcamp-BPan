1e363e4d79ff37c897b256aba8b13a381f3d0a2f
Padrao <padraogabriel20@gmail.com>
2023-03-09 21:43:34 -0300

    Novos Codigos

--- Projetos/Pj5.java
+++ Projetos/Pj5.java
@@ -1,11 +1,10 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-/*A seguinte sequência de números 0 1 1 2 3 5 8 13 21... é conhecida como série de Fibonacci. 
-Nessa sequência, cada número, depois dos 2 primeiros, é igual à soma dos 2 anteriores. Escreva um algoritmo que leia um inteiro N (N < 46) e 
-mostre os N primeiros números dessa série. */
+/*A seguinte sequência de números 0 1 1 2 3 5 8 13 21... é conhecida como série de Fibonacci. Nessa sequência, cada número, depois dos 2 primeiros, é igual à soma dos 2 anteriores. 
+Escreva um algoritmo que leia um inteiro N (N < 46) e mostre os N primeiros números dessa série. */
 
-public class Pj5{
+public class Pj5 {
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
         int N = leitor.nextInt();
